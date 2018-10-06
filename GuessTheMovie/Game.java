32a41bb77d639808489b3afd3521667937512799
Amit Khatri <amit.vkhatri@gmail.com>
2018-10-06 16:56:27 -0700

    Finished GuessTheMovie

--- GuessTheMovie/Game.java
+++ GuessTheMovie/Game.java
@@ -1,21 +1,43 @@
 import java.io.*;
 import java.util.*;
+import java.lang.StringBuilder;
 class Game{
   private String movieName, currentState;
-  private int guesses;
+  private int guessesLeft;
   private File inputFile;
   private Scanner scanner;
 
   Game() throws Exception{
     inputFile = new File("movies.txt");
     scanner = new Scanner(inputFile);
-    guesses = 0;
+    guessesLeft = 10;
     selectMovie();
     initState();
-    System.out.println(movieName);
-    System.out.println(currentState);
   }
 
+  public String getcurrentState(){
+    return currentState;
+  }
+
+  public int getguessesLeft(){
+    return guessesLeft;
+  }
+
+  public boolean guess(String letter){
+    boolean found = false;
+    StringBuilder builder = new StringBuilder(currentState);
+    for(int i = 0; i < movieName.length(); i++){
+      if(movieName.charAt(i) == letter.charAt(0)){
+        builder.setCharAt(i,letter.charAt(0));
+        currentState = builder.toString();
+        found = true;
+        guessesLeft--;
+      }
+    }
+    return found;
+  }
+
+  //Randomly select a movie
   private void selectMovie(){
     Random rand = new Random();
     int randomNumber = rand.nextInt(20);
@@ -24,6 +46,7 @@ class Game{
     }
   }
 
+  //Initialize game state
   private void initState(){
     currentState = "";
     for(int i = 0; i < movieName.length(); i++){
--- GuessTheMovie/Main.java
+++ GuessTheMovie/Main.java
@@ -1,6 +1,23 @@
-
+import java.io.*;
+import java.util.*;
 class Main{
   public static void main(String args[]) throws Exception{
     Game game = new Game();
+    Scanner scanner = new Scanner(System.in);
+    String guess;
+
+    System.out.println("Welcome to Guess The Movie");
+    System.out.println("You have " + game.getguessesLeft() + " guesses left, guess a letter\n");
+    System.out.println(game.getcurrentState());
+
+    while(game.getguessesLeft() > 0){
+      guess = scanner.nextLine();
+      if(game.guess(guess)){
+        System.out.println("The letter you entered exists!");
+        System.out.println(game.getcurrentState());
+      } else{
+        System.out.println("Guess again!");
+      }
+    }
   }
 }
